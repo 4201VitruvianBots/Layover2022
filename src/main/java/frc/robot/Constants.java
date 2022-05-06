4380a0e8d337c8b7b28ab0de288fba0831c2b907
jonathandao0 <jonathandao0@users.noreply.github.com>
2022-05-06 12:03:10 -0700

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,49 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+  
+  private final class USB {
+    public static final int leftJoystick = 0;
+    public static final int rightJoystick = 1;
+    public static final int xBoxContorller = 2;
+  }
+  
+  private final class CAN {
+    public static final int pidgeon = 9;
+    
+    public static final int frontLeftDriveMotor = 20;
+    public static final int frontLeftTurningMotor = 21;
+    public static final int frontRightDriveMotor = 22;
+    public static final int frontRightTurningMotor = 23;
+    public static final int backLeftDriveMotor = 24;
+    public static final int backLeftTurningMotor = 25;
+    public static final int backRightDriveMotor = 26;
+    public static final int backRightTurningMotor = 27;
+  }
+  
+  private final class SwerveDrive {
+    public static final double kTrackWidth = Units.inchesToMeters(30);
+    public static final double kWheelBase = Units.inchesToMeters(30);
+    
+    public static final double kMaxSpeedMetersPerSecond = Units.feetToMeters(18);
+    public static final double kMaxChassisRotationRadiansPerSecond = Math.PI * 1.5;
+  }
+  
+  private final class SwerveModule {
+    public static final double kDriveMotorGearRatio = 6.12;
+    public static final double kTurningMotorGearRatio = 12.8;
+    public static final int kFalconEncoderCPR = 2048;
+    public static final int kCANCoderCPR = 4096;
+    
+    public static final double kTurningMotorDistancePerPulse = 360.0 / kFalconEncoderCPR;
+    public static final double kTurningEncoderDistancePerPulse = 360.0 / kCANCoderCPR;
+
+    public static final double ksDriveVoltSecondsPerMeter = 0.667 / 12;
+    public static final double kvDriveVoltSecondsSquaredPerMeter = 2.44 / 12;
+    public static final double kaDriveVoltSecondsSquaredPerMeter = 0.27 / 12;
+
+    public static final double kvTurnVoltSecondsPerRadian = 1.47; // originally 1.5
+    public static final double kaTurnVoltSecondsSquaredPerRadian = 0.348; // originally 0.3
+  }
+}
