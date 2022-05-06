7e52c39e1b12bf64183083a36b201a056bd1328d
jonathandao0 <jonathandao0@users.noreply.github.com>
2022-05-06 12:10:31 -0700

    Create SwerveDrive.java

---
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -0,0 +1,28 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+public class SwerveDrive extends SubsystemBase {
+  
+  public SwerveDrive() {
+  }
+
+  private void updateSmartDashboard() {
+    
+  }
+  
+  @Override
+  public void periodic() {
+    
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    
+  }
+}
