f5274f1724c2ceb81891a3e7da9cc48fefa025ba
jonathandao0 <jonathandao0@users.noreply.github.com>
2022-05-06 12:11:45 -0700

    Create SwerveModule.java

---
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -0,0 +1,26 @@
+
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+public class SwerveModule extends SubsystemBase {
+  
+  public SwerveModule() {
+  }
+
+  private void updateSmartDashboard() {
+    
+  }
+  
+  @Override
+  public void periodic() {
+    
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    
+  }
+}
