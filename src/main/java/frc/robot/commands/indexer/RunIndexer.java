606aae3756f91b7b714b226a195602d9085d8416
Matthew Ochoa <matthewochoacoc@gmail.com>
2022-08-28 18:05:46 -0700

    Removed rollers

--- src/main/java/frc/robot/commands/indexer/RunIndexer.java
+++ src/main/java/frc/robot/commands/indexer/RunIndexer.java
@@ -35,7 +35,6 @@ public class RunIndexer extends CommandBase {
   @Override
   public void execute() {
     if (m_runKicker) m_indexer.setKickerPercentOutput(0.8);
-    else m_intake.setIntakeRollerPercentOutput(0.7);
 
     m_indexer.setIndexerPercentOutput(0.65);
   }
@@ -45,7 +44,6 @@ public class RunIndexer extends CommandBase {
   public void end(boolean interrupted) {
     m_indexer.setIndexerPercentOutput(0);
     if (m_runKicker) m_indexer.setKickerPercentOutput(0);
-    else m_intake.setIntakeRollerPercentOutput(0);
   }
 
   // Returns true when the command should end.
