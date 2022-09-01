4dae4856ff4ce858e9e307c3b39b3608e17a080b
Sheraz <Sheraz24132@gmail.com>
2022-08-31 17:53:11 -0700

    Spotless and build

--- src/main/java/frc/robot/commands/auto/FiveBallAuto.java
+++ src/main/java/frc/robot/commands/auto/FiveBallAuto.java
@@ -1,6 +1,5 @@
 package frc.robot.commands.auto;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import com.pathplanner.lib.commands.PPSwerveControllerCommand;
@@ -9,52 +8,16 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.subsystems.SwerveDrive;
 
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.RobotBase;
-import edu.wpi.first.wpilibj2.command.ConditionalCommand;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-// import frc.robot.Constants.DriveTrain.DriveTrainNeutralMode;
-// import frc.robot.commands.InterruptingCommand;
-// import frc.robot.commands.driveTrain.CargoTrajectoryRameseteCommand;
-// import frc.robot.commands.driveTrain.DriveToCargoTrajectory;
-// import frc.robot.commands.driveTrain.SetDriveTrainNeutralMode;
-// import frc.robot.commands.driveTrain.SetOdometry;
-// import frc.robot.commands.flywheel.SetAndHoldRpmSetpoint;
-// import frc.robot.commands.indexer.AutoRunIndexer;
-// import frc.robot.commands.intake.AutoRunIntake;
-// import frc.robot.commands.intake.AutoRunIntakeIndexer;
-// import frc.robot.commands.intake.AutoRunIntakeInstant;
-// import frc.robot.commands.intake.AutoRunIntakeOnly;
-// import frc.robot.commands.intake.IntakePiston;
-// import frc.robot.commands.simulation.SetSimTrajectory;
-// import frc.robot.commands.simulation.SimulationShoot;
-// import frc.robot.commands.turret.AutoUseVisionCorrection;
-// import frc.robot.commands.turret.SetTurretAbsoluteSetpointDegrees;
-// import frc.robot.simulation.FieldSim;
-// import frc.robot.subsystems.DriveTrain;
-// import frc.robot.subsystems.Flywheel;
-// import frc.robot.subsystems.Indexer;
-// import frc.robot.subsystems.Intake;
-// import frc.robot.subsystems.Turret;
-// import frc.robot.subsystems.Vision;
-// import frc.vitruvianlib.utils.TrajectoryUtils;
-
-
 public class FiveBallAuto extends SequentialCommandGroup {
   public FiveBallAuto(SwerveDrive swerveDrive) {
     PathPlannerTrajectory trajectory1 =
-        PathPlanner.loadPath(
-            "FiveBallAuto-1", Units.feetToMeters(2), Units.feetToMeters(2), false);
+        PathPlanner.loadPath("FiveBallAuto-1", Units.feetToMeters(2), Units.feetToMeters(2), false);
     PathPlannerTrajectory trajectory2 =
-        PathPlanner.loadPath(
-            "FiveBallAuto-2", Units.feetToMeters(2), Units.feetToMeters(2), false);
+        PathPlanner.loadPath("FiveBallAuto-2", Units.feetToMeters(2), Units.feetToMeters(2), false);
     PathPlannerTrajectory trajectory3 =
-        PathPlanner.loadPath(
-            "FiveBallAuto-3", Units.feetToMeters(2), Units.feetToMeters(2), false);
-    PathPlannerTrajectory trajectory4 = 
-        PathPlanner.loadPath(
-            "FiveBallAuto-4", Units.feetToMeters(2), Units.feetToMeters(2), false);
+        PathPlanner.loadPath("FiveBallAuto-3", Units.feetToMeters(2), Units.feetToMeters(2), false);
+    PathPlannerTrajectory trajectory4 =
+        PathPlanner.loadPath("FiveBallAuto-4", Units.feetToMeters(2), Units.feetToMeters(2), false);
     PPSwerveControllerCommand command1 =
         new PPSwerveControllerCommand(
             trajectory1,
@@ -75,7 +38,7 @@ public class FiveBallAuto extends SequentialCommandGroup {
             swerveDrive.getThetaPidController(),
             swerveDrive::setSwerveModuleStatesAuto,
             swerveDrive);
-    PPSwerveControllerCommand command3 = 
+    PPSwerveControllerCommand command3 =
         new PPSwerveControllerCommand(
             trajectory3,
             swerveDrive::getPoseMeters,
@@ -85,7 +48,7 @@ public class FiveBallAuto extends SequentialCommandGroup {
             swerveDrive.getThetaPidController(),
             swerveDrive::setSwerveModuleStatesAuto,
             swerveDrive);
-     PPSwerveControllerCommand command4 = 
+    PPSwerveControllerCommand command4 =
         new PPSwerveControllerCommand(
             trajectory4,
             swerveDrive::getPoseMeters,
@@ -100,14 +63,14 @@ public class FiveBallAuto extends SequentialCommandGroup {
         // new SetSimTrajectory(fieldSim, trajectory1, trajectory2, trajectory3),
         // new SetOdometry(driveTrain, fieldSim, trajectory1.getInitialPose()),
         // new SetDriveTrainNeutralMode(driveTrain, DriveTrainNeutralMode.BRAKE),
-        
-        // Path 1 + intake 1 cargo 
+
+        // Path 1 + intake 1 cargo
         // new IntakePiston(intake, true),
         // new SetAndHoldRpmSetpoint(flywheel, vision, 1600),
         // new ParallelDeadlineGroup(
-            //new InterruptingCommand(
-                 command1,//.andThen(() -> driveTrain.setMotorTankDrive(0, 0)),
-            // new AutoRunIntakeIndexer(intake, indexer),
+        // new InterruptingCommand(
+        command1, // .andThen(() -> driveTrain.setMotorTankDrive(0, 0)),
+        // new AutoRunIntakeIndexer(intake, indexer),
         //     new SetTurretAbsoluteSetpointDegrees(turret, 0)),
         // new IntakePiston(intake, false),
 
@@ -117,13 +80,13 @@ public class FiveBallAuto extends SequentialCommandGroup {
         //     new AutoRunIndexer(indexer, flywheel, 0.8).withTimeout(0.9),
         //     new SimulationShoot(fieldSim, true).withTimeout(0.9),
         //     RobotBase::isReal),
-        
+
         // // Path 2 + intake 1 cargo
         // new IntakePiston(intake, true),
         // new SetAndHoldRpmSetpoint(flywheel, vision, 1700),
         // new ParallelDeadlineGroup(
         //     new InterruptingCommand(
-                command2,//.andThen(() -> driveTrain.setMotorTankDrive(0, 0)),
+        command2, // .andThen(() -> driveTrain.setMotorTankDrive(0, 0)),
         //     new AutoRunIntake(intake, indexer),
         //     // need this (SetTurret command) here?
         //     new SetTurretAbsoluteSetpointDegrees(turret, 20)),
@@ -136,18 +99,20 @@ public class FiveBallAuto extends SequentialCommandGroup {
         //     new SimulationShoot(fieldSim, true).withTimeout(0.9),
         //     RobotBase::isReal),
 
-        // // Path 3 + run intake + wait for human player (collect 2 cargo) --> already getting general range of hub
+        // // Path 3 + run intake + wait for human player (collect 2 cargo) --> already getting
+        // general range of hub
         // new SetAndHoldRpmSetpoint(flywheel, vision, 1700),
         // new SetTurretAbsoluteSetpointDegrees(turret, 30).withTimeout(0.25),
         // new IntakePiston(intake, true),
         // new AutoRunIntakeInstant(intake, indexer, true),
-        //new ParallelDeadlineGroup(
-                command3,//.andThen(() -> driveTrain.setMotorTankDrive(0, 0))
-                // new AutoRunIntakeIndexer(intake, indexer).withTimeout(1)), // change this to 1.5? more time at the terminal (?)
+        // new ParallelDeadlineGroup(
+        command3, // .andThen(() -> driveTrain.setMotorTankDrive(0, 0))
+        // new AutoRunIntakeIndexer(intake, indexer).withTimeout(1)), // change this to 1.5? more
+        // time at the terminal (?)
         // new IntakePiston(intake, false),
 
         // // Path 4 + shoot 2
-        command4//.andThen(() -> driveTrain.setMotorTankDrive(0, 0)),
+        command4 // .andThen(() -> driveTrain.setMotorTankDrive(0, 0)),
         // new IntakePiston(intake, false),
         // new AutoUseVisionCorrection(turret, vision).withTimeout(0.75),
         // new ParallelDeadlineGroup(
@@ -156,6 +121,6 @@ public class FiveBallAuto extends SequentialCommandGroup {
         //         new SimulationShoot(fieldSim, true).withTimeout(5.0),
         //         RobotBase::isReal),
         //     new AutoRunIntakeOnly(intake))
-    );
+        );
   }
 }
